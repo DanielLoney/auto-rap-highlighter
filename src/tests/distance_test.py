e31fc9f37bb204add2a59e84d266b139bb73c53d
Daniel Loney <danieljloneyacc@gmail.com>
2021-05-17 17:35:46 +0800

    added more tests for distance

--- src/tests/distance_test.py
+++ src/tests/distance_test.py
@@ -1,7 +1,9 @@
 import unittest
 from aline import aline, arpa2aline
+import linkage
 
 class GroupingTests(unittest.TestCase):
+    san = ['S', 'AH', 'N']
 
     def test_arpa2aline(self):
         hustlin = arpa2aline.arpa2aline(['HH', 'AH', 'S', 'L', 'IH', 'N'])
@@ -14,16 +16,56 @@ class GroupingTests(unittest.TestCase):
 
     def test_align(self):
         self.assertListEqual(\
-            linkage.align(['S', 'AH', 'N'], ['S', 'AH', 'N']),\
-            [[('s', 's'), ('ʌ', 'ʌ'), ('n', 'n')]])
-
-        self.assertListEqual(\
-            linkage.align(['S', 'AH', 'N'], ['S', 'AH', 'N']),\
+            linkage.align(self.san, self.san),\
             [[('s', 's'), ('ʌ', 'ʌ'), ('n', 'n')]])
 
         # Alignment for all arpa and itself gives a tuple of its converted
         # IPA
-        for arpa in arpabet2aline_dict:
-            self.assertListEqual(\
-                linkage.align([arpa], [arpa]),\
-                [[(arpabet2aline_dict[arpa], arpabet2aline_dict[arpa])]])
+        d = arpa2aline.arpabet2aline_dict
+        for arpa in d:
+            alignment = [[(ipa, ipa) for ipa in d[arpa]]]
+            with self.subTest(arpa=arpa):
+                self.assertListEqual(\
+                    linkage.align([arpa], [arpa]),\
+                    alignment)
+
+    def test_distance_identity(self):
+        d1 = linkage.distance(self.san, self.san)
+        self.assertEqual(d1, 0)
+
+    def test_distance_diff_onsets(self):
+        san2 = self.san
+        with self.subTest():
+            zan = ['Z', 'AH', 'N']
+            d2 = linkage.distance(self.san, zan)
+            self.assertEqual(d2, 0)
+            d3 = linkage.distance(san2, zan)
+            self.assertEqual(d3, 0)
+        with self.subTest():
+            span = ['S', 'P', 'AH', 'N']
+            d2 = linkage.distance(self.san, span)
+            self.assertEqual(d2, 0)
+            d3 = linkage.distance(san2, span)
+            self.assertEqual(d3, 0)
+
+
+    # Identity lower than Near Rhymes lower than Non-Rhymes
+    def test_distance_identity_near_rhyme_non_rhyme(self):
+
+        d1 = linkage.distance(self.san, self.san)
+        son = ['S', 'AO', 'N']
+        d2 = linkage.distance(self.san, son)
+        it = ['IH', 'T']
+        d3 = linkage.distance(self.san, it)
+        # d(san, san) < d(san, son) < d(san, it)
+        self.assertTrue(d1 < d2 < d3)
+
+    # Test extraneous phonemes
+    def test_extraneous_coda(self):
+        an = ['AH', 'N']
+        ant = ['AH', 'N', 'T']
+        # d(an, ant) > 0
+        self.assertTrue(linkage.distance(an, ant) > 0)
+
+    # Triangle Inequality
+
