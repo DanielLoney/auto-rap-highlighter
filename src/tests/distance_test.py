ccd2eb122377760180344350a55904f9a2a2fa85
Daniel Loney <danieljloneyacc@gmail.com>
2021-05-17 16:49:56 +0800

    added linkage.align tests

--- src/tests/distance_test.py
+++ src/tests/distance_test.py
@@ -12,4 +12,18 @@ class GroupingTests(unittest.TestCase):
         self.assertListEqual(funkmaster, ['f', 'ʌ', 'ŋ', 'k', 'm', 'æ', 's',\
             't', 'ɝ'])
 
+    def test_align(self):
+        self.assertListEqual(\
+            linkage.align(['S', 'AH', 'N'], ['S', 'AH', 'N']),\
+            [[('s', 's'), ('ʌ', 'ʌ'), ('n', 'n')]])
 
+        self.assertListEqual(\
+            linkage.align(['S', 'AH', 'N'], ['S', 'AH', 'N']),\
+            [[('s', 's'), ('ʌ', 'ʌ'), ('n', 'n')]])
+
+        # Alignment for all arpa and itself gives a tuple of its converted
+        # IPA
+        for arpa in arpabet2aline_dict:
+            self.assertListEqual(\
+                linkage.align([arpa], [arpa]),\
+                [[(arpabet2aline_dict[arpa], arpabet2aline_dict[arpa])]])
