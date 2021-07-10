6fe2781eed606c6aac0ce72bb5242d976abab47f
Daniel Loney <danieljloneyacc@gmail.com>
2021-07-10 21:57:58 +0800

    more refactoring

--- src/clustering.py
+++ src/clustering.py
@@ -1,7 +1,8 @@
 from copy import deepcopy
+from collections import namedtuple
+
 import linkage
 from groups import Groups
-from collections import namedtuple
 
 MAX_CODA_CONSONANTS = 5
 MAX_ONSET_CONSONANTS = 3
@@ -9,6 +10,7 @@ MAX_ONSET_CONSONANTS = 3
 
 class _ClusterIterVars:
     """Object to represent variables required for _cluster_iteration"""
+
     def __init__(self):
         self.next_group_id = None
         self.groups = None
@@ -18,52 +20,10 @@ class _ClusterIterVars:
         self.verse_dict = None
         self.current_verse_groups = None
 
-def _update_live_groups(cluster_args, iter_vars, first_iter, line_number):
-    if first_iter:
-        # Update live_groups
-        new_live_groups = deepcopy(iter_vars.live_groups)
-        for _id in iter_vars.live_groups:
-            if not _still_live(
-                iter_vars.live_groups, line_number, cluster_args.max_live_lines, _id
-            ):
-                del new_live_groups[_id]
-        iter_vars.live_groups = new_live_groups
-    elif not first_iter:
-        # Update live_groups
-        iter_vars.live_groups = iter_vars.groups.get_groups_in_range(
-            line_number, cluster_args.max_live_lines
-        )
 
-def _reinsert_word(iter_vars, line_number, word_i, word):
-    """Remove all syllables from groups and remove the group
-    id associated with it if it was the only syllable left
-    in the group"""
-    for p_i, p in enumerate(word):
-        for s_i in range(len(p)):
-            index = (line_number, word_i, p_i, s_i)
-            if index in iter_vars.groups.index_to_group:
-                _id = iter_vars.groups.index_to_group[index]
-                iter_vars.groups.remove_syllable(index)
-                if _id not in iter_vars.groups.id_to_group:
-                    iter_vars.live_groups.remove(_id)
 
-def _save_and_reset(cluster_args, iter_vars, first_iter, line_number):
-    if cluster_args.verse_tracking:
-        # save
-        if len(iter_vars.current_verse_groups) > 0:
-            iter_vars.verse_dict[
-                iter_vars.current_verse_starting_line
-            ] = iter_vars.current_verse_groups
-
-        # reset
-        iter_vars.current_verse_starting_line = line_number + 1
-        iter_vars.current_verse_groups = set()
-    if first_iter:
-        # reset live_groups
-        iter_vars.live_groups = dict()
 def _cluster_iteration(cluster_args, iter_vars, first_iter=True):
     """Single cluster iteration, see cluster docstring for more info"""
-
 
     # Reset verse_tracking
     if cluster_args.verse_tracking:
@@ -72,6 +32,7 @@ def _cluster_iteration(cluster_args, iter_vars, first_iter=True):
         iter_vars.current_verse_groups = set()
 
     for line_number, line in enumerate(cluster_args.syllable_lines):
+        # List for each line
         if first_iter:
             iter_vars.final_pronunciations.append([])
 
@@ -92,54 +53,124 @@ def _cluster_iteration(cluster_args, iter_vars, first_iter=True):
                 _reinsert_word(iter_vars, line_number, word_i, word)
             # Get best pronunciation
             p_i = get_best_pronunciation(word, iter_vars.groups, iter_vars.live_groups)
+
             # Update final_pronunciations
             if first_iter:
                 iter_vars.final_pronunciations[line_number].append(p_i)
             else:
                 iter_vars.final_pronunciations[line_number][word_i] = p_i
+
             pronun = word[p_i]
             for syllable_i, syllable in enumerate(pronun):
                 # Check group linkage value of set([syllable]) and
                 # live_groups
-                sorted_base_syllable_linkages = _get_sorted_linkages(
-                    iter_vars.groups, syllable, iter_vars.live_groups
-                )
 
-                (
-                    best_group_id,
-                    best_linkage_value,
-                ) = _get_best_group_id_linkage_distance(sorted_base_syllable_linkages)
+                indexes = (line_number, word_i, p_i, syllable_i)
+                _check_syllable_and_update(cluster_args, iter_vars, first_iter,
+                        indexes, syllable)
 
-                if best_linkage_value <= cluster_args.linkage_criterion:
-                    # Add the syllable to the best_group_id
-                    # Update groups
-                    iter_vars.groups.add_syllable(
-                        best_group_id, (line_number, word_i, p_i, syllable_i)
-                    )
-                    if first_iter:
-                        # Update live_groups
-                        iter_vars.live_groups[best_group_id] = {
-                            "most_recent_line": line_number
-                        }
-                else:
-                    # print("Adding new group: {}".format(new_group))
-                    # Update groups
-                    iter_vars.groups.add_group(
-                        iter_vars.next_group_id,
-                        [(line_number, word_i, p_i, syllable_i)],
-                    )
-                    # Update live_groups
-                    if first_iter:
-                        iter_vars.live_groups[iter_vars.next_group_id] = {
-                            "most_recent_line": line_number
-                        }
-                    else:
-                        iter_vars.live_groups.add(iter_vars.next_group_id)
-                    if cluster_args.verse_tracking:
-                        # Add to verse groups
-                        iter_vars.current_verse_groups.add(iter_vars.next_group_id)
-                    iter_vars.next_group_id += 1
     iter_vars.groups.set_pronunciations(iter_vars.final_pronunciations)
+
+def _check_syllable_and_update(cluster_args, iter_vars, first_iter, indexes,
+        syllable):
+    (line_number, word_i, p_i, syllable_i) = indexes
+    sorted_base_syllable_linkages = _get_sorted_linkages(
+        iter_vars.groups, syllable, iter_vars.live_groups
+    )
+
+    (
+        best_group_id,
+        best_linkage_value,
+    ) = _get_best_group_id_linkage_distance(sorted_base_syllable_linkages)
+
+
+    if best_linkage_value <= cluster_args.linkage_criterion:
+        # Add the syllable to the best_group_id
+        # Update groups
+        iter_vars.groups.add_syllable(
+            best_group_id, (line_number, word_i, p_i, syllable_i)
+        )
+        if first_iter:
+            # Update live_groups
+            iter_vars.live_groups[best_group_id] = {
+                "most_recent_line": line_number
+            }
+    else:
+        _add_new_group_and_update(
+            cluster_args,
+            iter_vars,
+            first_iter,
+            indexes,
+        )
+
+def _update_live_groups(cluster_args, iter_vars, first_iter, line_number):
+    if first_iter:
+        # Update live_groups
+        new_live_groups = deepcopy(iter_vars.live_groups)
+        for _id in iter_vars.live_groups:
+            if not _still_live(
+                iter_vars.live_groups, line_number, cluster_args.max_live_lines, _id
+            ):
+                del new_live_groups[_id]
+        iter_vars.live_groups = new_live_groups
+    elif not first_iter:
+        # Update live_groups
+        iter_vars.live_groups = iter_vars.groups.get_groups_in_range(
+            line_number, cluster_args.max_live_lines
+        )
+
+
+def _reinsert_word(iter_vars, line_number, word_i, word):
+    """Remove all syllables from groups and remove the group
+    id associated with it if it was the only syllable left
+    in the group"""
+    for p_i, p in enumerate(word):
+        for s_i in range(len(p)):
+            index = (line_number, word_i, p_i, s_i)
+            if index in iter_vars.groups.index_to_group:
+                _id = iter_vars.groups.index_to_group[index]
+                iter_vars.groups.remove_syllable(index)
+                if _id not in iter_vars.groups.id_to_group:
+                    iter_vars.live_groups.remove(_id)
+
+
+def _save_and_reset(cluster_args, iter_vars, first_iter, line_number):
+    if cluster_args.verse_tracking:
+        # save
+        if len(iter_vars.current_verse_groups) > 0:
+            iter_vars.verse_dict[
+                iter_vars.current_verse_starting_line
+            ] = iter_vars.current_verse_groups
+
+        # reset
+        iter_vars.current_verse_starting_line = line_number + 1
+        iter_vars.current_verse_groups = set()
+    if first_iter:
+        # reset live_groups
+        iter_vars.live_groups = dict()
+
+
+def _add_new_group_and_update(cluster_args, iter_vars, first_iter, indexes):
+    """
+    (line_number, word_i, p_i, syllable_i) = indexes
+    """
+    (line_number, word_i, p_i, syllable_i) = indexes
+    # Update groups
+    iter_vars.groups.add_group(
+        iter_vars.next_group_id,
+        [(line_number, word_i, p_i, syllable_i)],
+    )
+    # Update live_groups
+    if first_iter:
+        iter_vars.live_groups[iter_vars.next_group_id] = {
+            "most_recent_line": line_number
+        }
+    else:
+        iter_vars.live_groups.add(iter_vars.next_group_id)
+    if cluster_args.verse_tracking:
+        # Add to verse groups
+        iter_vars.current_verse_groups.add(iter_vars.next_group_id)
+    iter_vars.next_group_id += 1
 
 
 # ClusterArgs namedtuple subclass of tuple
